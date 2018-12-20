ada7d45bada6e35fd90b48da6a59ea1a818d79e5
sunyufei <17326862126@163.com>
2018-12-20 11:29:51 +0800

    update

--- src/pic_gray_two_value/gray_two_value.py
+++ src/pic_gray_two_value/gray_two_value.py
@@ -7,3 +7,13 @@
 '''
 import cv2
 
+
+img = cv2.imread('4.jpg')
+
+# #灰度
+img = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+cv2.imwrite('4_gray.jpg',img)
+
+#二值化
+cv2.threshold(img,50,255,cv2.THRESH_BINARY)
+cv2.imwrite('4_two_value.jpg',img)
